1b4bcefb0cb19bb84d044f9ea8e1986d163ed354
konstantinbylbas <konstantinbylbas@gmail.com>
2024-07-08 14:54:01 +0300

    NAV - changed view

--- src/app/components/common/nav/Nav.tsx
+++ src/app/components/common/nav/Nav.tsx
@@ -3,12 +3,11 @@
 import { Link, useLocation } from 'react-router-dom';
 import './Nav.scss';
 import { ImageType } from '@app/types/image.type';
-import texts from './Nav.text';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { ScreenPlatformPath, ScreenType } from '@app/types/screen.type';
 
 interface iTab {
-    label: string;
+    img?: string;
     link?: string;
     tabs?: iTab[];
 }
@@ -18,14 +17,25 @@ export default function Nav() {
 
     const [isOpenSubmenu, setIsOpenSubmenu] = useState(false);
 
+    const subtabsRef = useRef<HTMLDivElement | null>(null);
+
     const portfolioTabs: iTab[] = [
-        { label: texts.summary, link: `/${ScreenType.PORTFOLIO}#summary` },
-        { label: texts.skills, link: `/${ScreenType.PORTFOLIO}#skills` },
-        { label: texts.examples, link: `/${ScreenType.PORTFOLIO}#examples` },
+        {
+            img: ImageType.HOME,
+            link: `/${ScreenType.PORTFOLIO}`,
+        },
+        {
+            img: ImageType.PERSON,
+            link: `/${ScreenType.PORTFOLIO}#summary`,
+        },
+        {
+            img: ImageType.BRIEFCASE,
+            link: `/${ScreenType.PORTFOLIO}#examples`,
+        },
     ];
     const platformTabs: iTab[] = [
         {
-            label: texts.generatorString,
+            img: ImageType.GENERATOR_STRING,
             link: `/${ScreenType.PLATFORM}/${ScreenPlatformPath.GENERATOR}/`,
         },
     ];
@@ -39,10 +49,26 @@ export default function Nav() {
             if (anchor) {
                 document.getElementById(anchor)?.scrollIntoView();
             } else {
-                window.scrollTo(0, 0);
+                console.log('No anchor, scrolling to top');
+            window.scrollTo(0, 0);
+            console.log('Scrolled to top');
             }
         }, 0);
     }, [location]);
+
+    function toggleSubtabs(subtabsMenu: HTMLDivElement | null): void {
+        if (!subtabsMenu) {
+            return;
+        }
+
+        if (isOpenSubmenu) {
+            subtabsMenu.style.height = '0px';
+        } else {
+            subtabsMenu.style.height = subtabsMenu.scrollHeight + 'px';
+        }
+
+        setIsOpenSubmenu(!isOpenSubmenu);
+    }
 
     function updateTabs(): iTab[] {
         const appSection = location.pathname.split('/')[1] as ScreenType;
@@ -51,69 +77,61 @@ export default function Nav() {
             case ScreenType.PORTFOLIO:
                 return [
                     ...portfolioTabs,
-                    { label: texts.platform, tabs: platformTabs },
+                    {
+                        img: ImageType.PLATFORM,
+                        tabs: platformTabs,
+                    },
                 ];
 
             case ScreenType.PLATFORM:
                 return [
                     ...platformTabs,
-                    { label: texts.portfolio, tabs: portfolioTabs },
+                    { img: ImageType.LOGO, tabs: portfolioTabs },
                 ];
         }
     }
 
     return (
         <nav>
-            <div className="container">
-                <Link to="/" className="logo">
-                    <img src={ImageType.LOGO} alt="logo" />
-                </Link>
+            {tabs.map((tab, i) =>
+                tab.link ? (
+                    <Link to={tab.link} key={`portfolio tab #${i}`}>
+                        <img src={tab.img} />
+                    </Link>
+                ) : tab.tabs ? (
+                    <div className="tabs_subTabs" key={`portfolio tab #${i}`}>
+                        <button
+                            className={`tabs_subTabs_button ${isOpenSubmenu ? 'active' : ''}`}
+                            onClick={() => toggleSubtabs(subtabsRef.current)}>
+                            <img src={tab.img} />
+                        </button>
 
-                <div className="tabs">
-                    {tabs.map((tab, i) =>
-                        tab.link ? (
-                            <Link to={tab.link} key={`portfolio tab #${i}`}>
-                                {tab.label}
-                            </Link>
-                        ) : tab.tabs ? (
-                            <div
-                                className="tabs_button"
-                                onClick={() =>
-                                    setIsOpenSubmenu(!isOpenSubmenu)
-                                }>
-                                <p>{tab.label}</p>
-                                <span
-                                    className={`arrow ${isOpenSubmenu ? 'active' : ''}`}></span>
-
-                                <div
-                                    className={`tabs_subTabs-menu ${isOpenSubmenu ? 'active' : ''}`}
-                                    onClick={event => {
-                                        event.stopPropagation();
-                                    }}>
-                                    {tab.tabs.map((subTab, k) =>
-                                        subTab.link ? (
-                                            <Link
-                                                to={subTab.link}
-                                                onClick={event => {
-                                                    setIsOpenSubmenu( 
-                                                        !isOpenSubmenu,
-                                                    );
-                                                }}
-                                                key={`portfolio subTab #${k}`}>
-                                                {subTab.label}
-                                            </Link>
-                                        ) : (
-                                            ''
-                                        ),
-                                    )}
-                                </div>
-                            </div>
-                        ) : (
-                            ''
-                        ),
-                    )}
-                </div>
-            </div>
+                        <div
+                            className={`tabs_subTabs_menu ${isOpenSubmenu ? 'active' : ''}`}
+                            onClick={event => {
+                                event.stopPropagation();
+                            }}
+                            ref={subtabsRef}>
+                            {tab.tabs.map((subTab, k) =>
+                                subTab.link ? (
+                                    <Link
+                                        to={subTab.link}
+                                        onClick={() => {
+                                            setIsOpenSubmenu(!isOpenSubmenu);
+                                        }}
+                                        key={`portfolio subTab #${k}`}>
+                                        <img src={subTab.img} />
+                                    </Link>
+                                ) : (
+                                    ''
+                                ),
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    ''
+                ),
+            )}
         </nav>
     );
 }
