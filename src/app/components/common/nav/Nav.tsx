04a70320bdc5e4b3ea7f24deab01591772ee684b
konstantinbylbas <konstantinbylbas@gmail.com>
2024-11-07 10:14:35 +0200

    App - Improved seo to use dynamic meta for each page

--- src/app/components/common/nav/Nav.tsx
+++ src/app/components/common/nav/Nav.tsx
@@ -6,6 +6,12 @@ import { useEffect, useMemo } from 'react';
 import { ScreenType } from '@app/types/screen.type';
 import PortfolioNav from './components/portfolio/PortfolioNav';
 import PlatformNav from './components/platform/PlatformNav';
+import SeoJson from '../../../../seo.json';
+
+interface iSeoPageData {
+    title: string;
+    description: string;
+}
 
 export default function Nav() {
     const location = useLocation();
@@ -18,6 +24,8 @@ export default function Nav() {
     useEffect(() => {
         const anchor = location.hash.slice(1);
 
+        metaUpdater();
+
         setTimeout(() => {
             if (anchor) {
                 document.getElementById(anchor)?.scrollIntoView();
@@ -26,6 +34,58 @@ export default function Nav() {
             }
         }, 0);
     }, [location]);
+
+    function metaUpdater(): void {
+        const path = location.pathname;
+        const data = getPageSeoDataFromPath(path);
+        setPageSeoData(data);
+    }
+
+    function getPageSeoDataFromPath(path: string): iSeoPageData {
+        if (!path) {
+            return SeoJson.portfolio;
+        } else {
+            const [_, chapter, subChapter, page] = path.split('/');
+
+            if (!chapter || !subChapter || !page) {
+                return SeoJson.portfolio;
+            }
+
+            return (
+                (SeoJson as any)[chapter]?.chapters?.[subChapter]?.[page] ||
+                SeoJson.portfolio
+            );
+        }
+    }
+
+    function setPageSeoData(data: iSeoPageData): void {
+        const { title, description } = data;
+
+        document.title = title;
+
+        const metaOgTitle = document.querySelector('meta[name="og:title"]');
+        metaOgTitle?.setAttribute("content", title);
+
+        const metaTwitterTitle = document.querySelector(
+            'meta[name="twitter:title"]',
+        );
+        metaTwitterTitle?.setAttribute("content", title);
+
+        const metaDescription = document.querySelector(
+            'meta[name="description"]',
+        );
+        metaDescription?.setAttribute("content", description);
+
+        const metaOgDescription = document.querySelector(
+            'meta[name="og:description"]',
+        );
+        metaOgDescription?.setAttribute("content", description);
+
+        const metaTwitterDescription = document.querySelector(
+            'meta[name="twitter:description"]',
+        );
+        metaTwitterDescription?.setAttribute("content", description);
+    }
 
     return <nav>{isPortfolioSection ? <PortfolioNav /> : <PlatformNav />}</nav>;
 }
--- src/app/components/pages/platform/game/Game.tsx
+++ src/app/components/pages/platform/game/Game.tsx
@@ -4,6 +4,7 @@ import { useHistory, useLocation, useParams } from 'react-router-dom';
 import './Game.scss';
 import { useLayoutEffect } from 'react';
 import NaughtsAndCrosses from './naughts-and-crosses/NaughtsAndCrosses';
+import { GamePages } from '@app/types/screen.type';
 
 export default function Game() {
     const location = useLocation();
@@ -11,15 +12,15 @@ export default function Game() {
     const { id } = useParams<{ id?: string }>();
 
     useLayoutEffect(() => {
-        if (!id && !location.pathname.includes('/naughts-and-crosses')) {
-            history.push(`${location.pathname}/naughts-and-crosses`);
+        if (!id && !location.pathname.includes(GamePages.NAUGHTS_AND_CROSSES)) {
+            history.push(`${location.pathname}/${GamePages.NAUGHTS_AND_CROSSES}`);
         }
     }, [history, id, location.pathname]);
 
     return (
         <div className="game">
             <div className="container">
-                {id === 'naughts-and-crosses' ? <NaughtsAndCrosses /> : ''}
+                {id === GamePages.NAUGHTS_AND_CROSSES ? <NaughtsAndCrosses /> : ''}
             </div>
         </div>
     );
